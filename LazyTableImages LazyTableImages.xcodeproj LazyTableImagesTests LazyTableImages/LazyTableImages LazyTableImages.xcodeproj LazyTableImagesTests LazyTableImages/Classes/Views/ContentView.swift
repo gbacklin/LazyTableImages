67cac9882138aba3f7a0674d129b52de619294be
Gene Backlin <gbacklin@marizack.com>
2021-06-22 10:15:48 -0500

    Slide not needed

--- LazyTableImages LazyTableImages.xcodeproj LazyTableImagesTests LazyTableImages/LazyTableImages LazyTableImages.xcodeproj LazyTableImagesTests LazyTableImages/Classes/Views/ContentView.swift
+++ LazyTableImages LazyTableImages.xcodeproj LazyTableImagesTests LazyTableImages/LazyTableImages LazyTableImages.xcodeproj LazyTableImagesTests LazyTableImages/Classes/Views/ContentView.swift
@@ -25,7 +25,6 @@ struct ContentView: View {
                                     image
                                         .resizable()
                                         .scaledToFill()
-                                        .transition(.slide)
                                 case .failure(_):
                                     Color.gray.opacity(0.1)
                                 @unknown default:
